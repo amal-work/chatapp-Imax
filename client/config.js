807d283a0ad71ef979cff15fcbf9957f1957637c
amal <amalsudharika1@gmail.com>
2023-07-01 20:59:45 +0900

    rebuild

--- client/config.js
+++ client/config.js
@@ -2,6 +2,6 @@ const isDev = process.env.NODE_ENV === 'development';
 
 export default {
   isDev,
-  brandName: 'IMax11111',
+  brandName: 'IMax',
   avatarUploadLimit: 2 * 1024 * 1024, // 2 MB
 };
